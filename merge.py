9bf30b8fc622d2ae58fc02e330ca271a3ed23a6e
Mark Angelo Nool <markangelonool0@gmail.com>
2024-03-14 19:09:00 +0800

    update merge.py file

--- merge.py
+++ merge.py
@@ -6,3 +6,7 @@ def merge_sort(array):
     mid = len(array)//2
     left_half = array[:mid]
     right_half = array[mid:]
+
+    return merge(merge_sort(left_half), merge_sort(right_half))
+
+def merge(left,right):
