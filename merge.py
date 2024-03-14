303b436ecce26c890da9235b7fd53a13b13d0256
Mark Angelo Nool <markangelonool0@gmail.com>
2024-03-14 19:27:34 +0800

    update to code

--- merge.py
+++ merge.py
@@ -10,3 +10,31 @@ def merge_sort(array):
     return merge(merge_sort(left_half), merge_sort(right_half))
 
 def merge(left,right):
+    merged =[]
+    left_index = 0
+    right_index = 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] <= right[right_index]:
+            merged.append(left[left_index])
+            left_index +=1
+        else:
+            merged.append((right[right_index]))
+            right_index +=1
+
+    merged.extend(left[left_index:])
+    merged.extend(right[right_index:])\
+
+    return merged
+
+user_input = input("Enter numbers comma separated: ")
+array = list(map(int, user_input.split(',')))
+
+print(" ")
+print("unsorted list: ", (array))
+print("length of the list is: ",len(array))
+print("sorted list: ", merge_sort(array))
+if (len(array) % 2) == 0:
+    print("the array is even")
+else:
+    print("the array is odd")
